ecde5003c89d93d00cc489c3c9fee07fd7d5b022
Eugene Lazin <4lazin@gmail.com>
2020-02-26 14:47:06 +0300

    WIP

--- gen.sh
+++ gen.sh
@@ -3,13 +3,23 @@
 # Every file has 125000 series
 # Total number of data points - 525.6B
 
-python2.7 gen.py --rbegin=0      --rend=125000  --tbegin=20190101T000000 --tend=20200101T000000 --tdelta=00:01:00 | gzip > 525K_points_125000_columns_60sec_step_0.gz &
-python2.7 gen.py --rbegin=125000 --rend=250000  --tbegin=20190101T000000 --tend=20200101T000000 --tdelta=00:01:00 | gzip > 525K_points_125000_columns_60sec_step_1.gz &
-python2.7 gen.py --rbegin=250000 --rend=375000  --tbegin=20190101T000000 --tend=20200101T000000 --tdelta=00:01:00 | gzip > 525K_points_125000_columns_60sec_step_2.gz &
-python2.7 gen.py --rbegin=375000 --rend=500000  --tbegin=20190101T000000 --tend=20200101T000000 --tdelta=00:01:00 | gzip > 525K_points_125000_columns_60sec_step_3.gz &
-python2.7 gen.py --rbegin=500000 --rend=625000  --tbegin=20190101T000000 --tend=20200101T000000 --tdelta=00:01:00 | gzip > 525K_points_125000_columns_60sec_step_4.gz &
-python2.7 gen.py --rbegin=625000 --rend=750000  --tbegin=20190101T000000 --tend=20200101T000000 --tdelta=00:01:00 | gzip > 525K_points_125000_columns_60sec_step_5.gz &
-python2.7 gen.py --rbegin=750000 --rend=875000  --tbegin=20190101T000000 --tend=20200101T000000 --tdelta=00:01:00 | gzip > 525K_points_125000_columns_60sec_step_6.gz &
-python2.7 gen.py --rbegin=875000 --rend=1000000 --tbegin=20190101T000000 --tend=20200101T000000 --tdelta=00:01:00 | gzip > 525K_points_125000_columns_60sec_step_7.gz &
+#python2.7 gen.py --rbegin=0      --rend=125000  --tbegin=20190101T000000 --tend=20200101T000000 --tdelta=00:01:00 | gzip > 525K_points_125000_columns_60sec_step_0.gz &
+#python2.7 gen.py --rbegin=125000 --rend=250000  --tbegin=20190101T000000 --tend=20200101T000000 --tdelta=00:01:00 | gzip > 525K_points_125000_columns_60sec_step_1.gz &
+#python2.7 gen.py --rbegin=250000 --rend=375000  --tbegin=20190101T000000 --tend=20200101T000000 --tdelta=00:01:00 | gzip > 525K_points_125000_columns_60sec_step_2.gz &
+#python2.7 gen.py --rbegin=375000 --rend=500000  --tbegin=20190101T000000 --tend=20200101T000000 --tdelta=00:01:00 | gzip > 525K_points_125000_columns_60sec_step_3.gz &
+#python2.7 gen.py --rbegin=500000 --rend=625000  --tbegin=20190101T000000 --tend=20200101T000000 --tdelta=00:01:00 | gzip > 525K_points_125000_columns_60sec_step_4.gz &
+#python2.7 gen.py --rbegin=625000 --rend=750000  --tbegin=20190101T000000 --tend=20200101T000000 --tdelta=00:01:00 | gzip > 525K_points_125000_columns_60sec_step_5.gz &
+#python2.7 gen.py --rbegin=750000 --rend=875000  --tbegin=20190101T000000 --tend=20200101T000000 --tdelta=00:01:00 | gzip > 525K_points_125000_columns_60sec_step_6.gz &
+#python2.7 gen.py --rbegin=875000 --rend=1000000 --tbegin=20190101T000000 --tend=20200101T000000 --tdelta=00:01:00 | gzip > 525K_points_125000_columns_60sec_step_7.gz &
+#wait
+
+python2.7 gen.py --rbegin=0      --rend=125000  --tbegin=20190101T000000 --tend=20190102T000000 --tdelta=00:01:00 | gzip > 525K_points_125000_columns_60sec_step_0.gz &
+python2.7 gen.py --rbegin=125000 --rend=250000  --tbegin=20190101T000000 --tend=20190102T000000 --tdelta=00:01:00 | gzip > 525K_points_125000_columns_60sec_step_1.gz &
+python2.7 gen.py --rbegin=250000 --rend=375000  --tbegin=20190101T000000 --tend=20190102T000000 --tdelta=00:01:00 | gzip > 525K_points_125000_columns_60sec_step_2.gz &
+python2.7 gen.py --rbegin=375000 --rend=500000  --tbegin=20190101T000000 --tend=20190102T000000 --tdelta=00:01:00 | gzip > 525K_points_125000_columns_60sec_step_3.gz &
+python2.7 gen.py --rbegin=500000 --rend=625000  --tbegin=20190101T000000 --tend=20190102T000000 --tdelta=00:01:00 | gzip > 525K_points_125000_columns_60sec_step_4.gz &
+python2.7 gen.py --rbegin=625000 --rend=750000  --tbegin=20190101T000000 --tend=20190102T000000 --tdelta=00:01:00 | gzip > 525K_points_125000_columns_60sec_step_5.gz &
+python2.7 gen.py --rbegin=750000 --rend=875000  --tbegin=20190101T000000 --tend=20190102T000000 --tdelta=00:01:00 | gzip > 525K_points_125000_columns_60sec_step_6.gz &
+python2.7 gen.py --rbegin=875000 --rend=1000000 --tbegin=20190101T000000 --tend=20190102T000000 --tdelta=00:01:00 | gzip > 525K_points_125000_columns_60sec_step_7.gz &
 wait
 
--- run.sh
+++ run.sh
@@ -8,7 +8,8 @@ function wait_for_completion() {
     c=0
     printf "Waiting until write will be completed "
     until [ -n "$response" ]; do
-        response=$(curl -s --url $AKUMULI_ENDPOINT:8181/api/query -d "{ \"select\": \"temperature_celsius\", \"where\" : { \"sensor_id\": \"$1\" }, \"range\": { \"from\": \"20191231T235800.000000\", \"to\": \"20200101T000000.000000\" }}")
+        response=$(curl -s --url $AKUMULI_ENDPOINT:8181/api/query -d "{ \"select\": \"temperature_celsius\", \"where\" : { \"sensor_id\": \"$1\" }, \"range\": { \"from\": \"20190101T235800.000000\", \"to\": \"20200101T000000.000000\" }}")
+        #response=$(curl -s --url $AKUMULI_ENDPOINT:8181/api/query -d "{ \"select\": \"temperature_celsius\", \"where\" : { \"sensor_id\": \"$1\" }, \"range\": { \"from\": \"20191231T235800.000000\", \"to\": \"20200101T000000.000000\" }}")
         printf '.'
         sleep 1
         ((c++)) && ((c==20)) && break
@@ -64,26 +65,50 @@ timestamp=$(date +%Y%m%dT%H%M%S)
 
 echo "Run aggregate query 1-thread @ $timestamp"
 
-time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": "where": {"group":0}, { "temperature_celsius": "max" } }' | wc -c
-time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": "where": {"group":1}, { "temperature_celsius": "max" } }' | wc -c
-time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": "where": {"group":2}, { "temperature_celsius": "max" } }' | wc -c
-time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": "where": {"group":3}, { "temperature_celsius": "max" } }' | wc -c
-time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": "where": {"group":4}, { "temperature_celsius": "max" } }' | wc -c
-time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": "where": {"group":5}, { "temperature_celsius": "max" } }' | wc -c
-time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": "where": {"group":6}, { "temperature_celsius": "max" } }' | wc -c
-time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": "where": {"group":7}, { "temperature_celsius": "max" } }' | wc -c
-time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": "where": {"group":8}, { "temperature_celsius": "max" } }' | wc -c
-time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": "where": {"group":9}, { "temperature_celsius": "max" } }' | wc -c
-time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": "where": {"group":10}, { "temperature_celsius": "max" } }' | wc -c
-time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": "where": {"group":11}, { "temperature_celsius": "max" } }' | wc -c
+time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": "where": {"group":"0"}, { "temperature_celsius": "max" } }' | wc -c
+time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": "where": {"group":"1"}, { "temperature_celsius": "max" } }' | wc -c
+time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": "where": {"group":"2"}, { "temperature_celsius": "max" } }' | wc -c
+time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": "where": {"group":"3"}, { "temperature_celsius": "max" } }' | wc -c
+time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": "where": {"group":"4"}, { "temperature_celsius": "max" } }' | wc -c
+time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": "where": {"group":"5"}, { "temperature_celsius": "max" } }' | wc -c
+time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": "where": {"group":"6"}, { "temperature_celsius": "max" } }' | wc -c
+time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": "where": {"group":"7"}, { "temperature_celsius": "max" } }' | wc -c
+time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": "where": {"group":"8"}, { "temperature_celsius": "max" } }' | wc -c
+time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": "where": {"group":"9"}, { "temperature_celsius": "max" } }' | wc -c
+time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": "where": {"group":"10"}, { "temperature_celsius": "max" } }' | wc -c
+time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": "where": {"group":"11"}, { "temperature_celsius": "max" } }' | wc -c
 wait
 
 timestamp=$(date +%Y%m%dT%H%M%S)
 echo "Completed @ $timestamp"
 
 sleep 600
+
+# Filter by value 1 thread
 timestamp=$(date +%Y%m%dT%H%M%S)
-echo "Run aggregate query @ $timestamp"
-time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "select": "cpu.user", "range": {"from": "20010101T000000", "to": "20300101T000000"}, "order-by": "series", "filter": { "gt": 10000 } }' | wc -c
+echo "Run filter query @ $timestamp"
+time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "select": "temperature_celsius", "range": {"from": "20010101T000000", "to": "20300101T000000"}, "order-by": "series", "filter": { "gt": 10000 } }' | wc -c
 timestamp=$(date +%Y%m%dT%H%M%S)
 echo "Completed @ $timestamp"
+
+sleep 600
+
+# Filter by value all threads
+timestamp=$(date +%Y%m%dT%H%M%S)
+echo "Run filter query @ $timestamp"
+time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "select": "temperature_celsius", "where": { "group": "0" }, "range": {"from": "20010101T000000", "to": "20300101T000000"}, "order-by": "series", "filter": { "gt": 10000 } }' | wc -c
+time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "select": "temperature_celsius", "where": { "group": "1" }, "range": {"from": "20010101T000000", "to": "20300101T000000"}, "order-by": "series", "filter": { "gt": 10000 } }' | wc -c
+time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "select": "temperature_celsius", "where": { "group": "2" }, "range": {"from": "20010101T000000", "to": "20300101T000000"}, "order-by": "series", "filter": { "gt": 10000 } }' | wc -c
+time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "select": "temperature_celsius", "where": { "group": "3" }, "range": {"from": "20010101T000000", "to": "20300101T000000"}, "order-by": "series", "filter": { "gt": 10000 } }' | wc -c
+time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "select": "temperature_celsius", "where": { "group": "4" }, "range": {"from": "20010101T000000", "to": "20300101T000000"}, "order-by": "series", "filter": { "gt": 10000 } }' | wc -c
+time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "select": "temperature_celsius", "where": { "group": "5" }, "range": {"from": "20010101T000000", "to": "20300101T000000"}, "order-by": "series", "filter": { "gt": 10000 } }' | wc -c
+time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "select": "temperature_celsius", "where": { "group": "6" }, "range": {"from": "20010101T000000", "to": "20300101T000000"}, "order-by": "series", "filter": { "gt": 10000 } }' | wc -c
+time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "select": "temperature_celsius", "where": { "group": "7" }, "range": {"from": "20010101T000000", "to": "20300101T000000"}, "order-by": "series", "filter": { "gt": 10000 } }' | wc -c
+time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "select": "temperature_celsius", "where": { "group": "8" }, "range": {"from": "20010101T000000", "to": "20300101T000000"}, "order-by": "series", "filter": { "gt": 10000 } }' | wc -c
+time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "select": "temperature_celsius", "where": { "group": "9" }, "range": {"from": "20010101T000000", "to": "20300101T000000"}, "order-by": "series", "filter": { "gt": 10000 } }' | wc -c
+time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "select": "temperature_celsius", "where": { "group": "10" }, "range": {"from": "20010101T000000", "to": "20300101T000000"}, "order-by": "series", "filter": { "gt": 10000 } }' | wc -c
+time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "select": "temperature_celsius", "where": { "group": "11" }, "range": {"from": "20010101T000000", "to": "20300101T000000"}, "order-by": "series", "filter": { "gt": 10000 } }' | wc -c
+wait
+timestamp=$(date +%Y%m%dT%H%M%S)
+echo "Completed @ $timestamp"
+
