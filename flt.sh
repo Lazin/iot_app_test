693ac5748e263fbbda6937d5f2ddd85d69a26854
Eugene Lazin <4lazin@gmail.com>
2020-03-12 10:11:36 +0300

    Update flt.sh

--- flt.sh
+++ flt.sh
@@ -1,9 +1,6 @@
 AKUMULI_ENDPOINT=${AKUMULI_ENDPOINT:-"127.0.0.1"}
 
-
 echo "Akumuli endpoint: $AKUMULI_ENDPOINT"
-
-
 
 # Aggregate using one thread
 timestamp=$(date +%Y%m%dT%H%M%S)
@@ -39,26 +36,6 @@ wait
 timestamp=$(date +%Y%m%dT%H%M%S)
 echo "Completed @ $timestamp"
 sleep 600
-
-# Aggregate using 10 threads
-timestamp=$(date +%Y%m%dT%H%M%S)
-echo "Run aggregate query in 5 threads @ $timestamp"
-time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": { "temp0": "max" } }' | wc -c &
-time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": { "temp1": "max" } }' | wc -c &
-time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": { "temp2": "max" } }' | wc -c &
-time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": { "temp3": "max" } }' | wc -c &
-time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": { "temp4": "max" } }' | wc -c &
-wait
-time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": { "temp5": "max" } }' | wc -c &
-time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": { "temp6": "max" } }' | wc -c &
-time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": { "temp7": "max" } }' | wc -c &
-time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": { "temp8": "max" } }' | wc -c &
-time curl -s -XPOST "http://$AKUMULI_ENDPOINT:8181/api/query" -d '{ "aggregate": { "temp9": "max" } }' | wc -c &
-wait
-timestamp=$(date +%Y%m%dT%H%M%S)
-echo "Completed @ $timestamp"
-sleep 600
-
 
 
 # Filter by value 10 threads
